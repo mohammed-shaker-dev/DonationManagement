3c3f4ba778ff1cefb162d11142e6dde825801564
Mohammed Shaker <114334170+mohammed-shaker-dev@users.noreply.github.com>
2025-02-02 22:12:55 +0400

    Dashboard project

---
+++ Dashboard/Dashboard.Core/SyncedAggregates/User.cs
@@ -0,0 +1,25 @@
+﻿using SharedKernel;
+using SharedKernel.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dashboard.Core.SyncedAggregates
+{
+    public class User :BaseEntity<long>,IAggregateRoot
+    {
+        public User(string fullName,string emailAddress)
+        {
+            FullName = fullName;
+            EmailAddress = emailAddress;
+        }
+        public string FullName { get; private set; }
+        public string EmailAddress { get; private set; }
+        public override string ToString()
+        {
+            return FullName.ToString();
+        }
+    }
+}
