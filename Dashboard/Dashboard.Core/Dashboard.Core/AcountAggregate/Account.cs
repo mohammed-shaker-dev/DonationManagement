7a4759ee1bfa62034aa616aaacbe768e0edaac2b
Mohammed Shaker <Mohammed.shaker@firstscreen.com>
2025-02-03 16:45:43 +0400

    update cor porject folder

---
+++ Dashboard/Dashboard.Core/Dashboard.Core/AcountAggregate/Account.cs
@@ -0,0 +1,14 @@
+﻿using SharedKernel;
+using SharedKernel.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dashboard.Core.BudgetAggregate
+{
+    public class Account: BaseEntity<long>,IAggregateRoot
+    {
+    }
+}
--- SharedKernel/SharedKernel/obj/Debug/net8.0/SharedKernel.AssemblyInfo.cs
+++ SharedKernel/SharedKernel/obj/Debug/net8.0/SharedKernel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SharedKernel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79a5e44394ed0fb2c6f9eb7b98d2903b4fd9b0ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SharedKernel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SharedKernel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
